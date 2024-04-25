a435628b00766c1ffabc93f1299003bec498e354
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-25 09:59:52 -0500

    38. Colores de la consola

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 const argv = require('./config/yargs');
+require('colors');
 
 console.clear();
 
 crearArchivo(argv.b, argv.l)
-	.then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+	.then((nombreArchivo) => console.log(nombreArchivo.rainbow, 'creado'))
 	.catch((err) => console.log(err));
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
+const colors = require('colors');
 
 const crearArchivo = async (base = 5, listar = false) => {
 	try {
 		let salida = '';
 
 		for (let i = 1; i <= 10; i++) {
-			salida += `${base} x ${i} = ${base * i}\n`;
+			salida += `${base} ${'x'.green} ${i} ${'='.green} ${base * i}\n`;
 		}
 
 		if (listar) {
-			console.log('===================');
-			console.log('   Tabla del:', base);
-			console.log('===================');
+			console.log('==================='.green);
+			console.log('   Tabla del:'.green, colors.blue(base));
+			console.log('==================='.green);
 			console.log(salida);
 		}
 
