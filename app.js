6209ed84143c8483296362b39c8c0b01384a3623
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-24 21:55:14 -0500

    33. Recibir información desde línea de comando

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
 
-const base = 6;
+const [, , arg3 = 'base=5'] = process.argv;
+
+const [, base = 5] = arg3.split('=');
+
+console.log(base);
+
+// const base = 6;
 
 crearArchivo(base)
 	.then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
