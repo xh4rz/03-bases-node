404b0d45e1c7009410dfb1516ebbea2e4d4b7218
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-25 10:22:22 -0500

    40. Git - Preparar repositorio

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -18,7 +18,7 @@ const crearArchivo = async (base = 5, listar = false, hasta = 10) => {
 			console.log(consola);
 		}
 
-		fs.writeFileSync(`tabla-${base}.txt`, salida);
+		fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
 
 		return `tabla-${base}.txt`;
 	} catch (err) {
