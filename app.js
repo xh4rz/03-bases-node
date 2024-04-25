205d767ddb6b80a329ebdd23390ae580dea59aec
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-24 22:18:13 -0500

    35. Yargs

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
 
 console.clear();
 
-const [, , arg3 = 'base=5'] = process.argv;
+console.log(process.argv);
+console.log(argv);
 
-const [, base = 5] = arg3.split('=');
+console.log('base: yargs', argv.base);
 
-console.log(base);
+// const [, , arg3 = 'base=5'] = process.argv;
+
+// const [, base = 5] = arg3.split('=');
 
 // const base = 6;
 
-crearArchivo(base)
-	.then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
-	.catch((err) => console.log(err));
+// crearArchivo(base)
+// 	.then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+// 	.catch((err) => console.log(err));
